7bed57532b4a35c5396c0bb71625afb99cee1d02
logicallaw <logicallawbio@gmail.com>
2025-05-11 16:13:17 +0900

    test: add comparator_test.cpp

---
+++ test/comparator_test.cpp
@@ -0,0 +1,70 @@
+/*
+ * This is file of the project 2025_Lecture_Algorithm
+ * Licensed under the MIT License.
+ * Copyright (c) 2025 Junho Kim
+ * For full license text, see the LICENSE file in the root directory or at
+ * https://opensource.org/license/mit
+ * Author: Junho Kim
+ * Latest Updated Date: 2025-05-11
+ */
+#include "../src/non_main_red_black_tree.cpp"
+#include <gtest/gtest.h>
+
+TEST(RBTComparator, Test1) {
+  pair<int, string> comp1(12200795, "Algorithm");
+  pair<int, string> comp2(12210795, "Misaso");
+
+  RBT *rbt = new RBT();
+  int result = rbt->comparator(comp1, comp2);
+  EXPECT_EQ(result, 1);
+}
+
+TEST(RBTComparator, Test2) {
+  pair<int, string> comp1(12220795, "Algorithm");
+  pair<int, string> comp2(12210795, "Misaso");
+
+  RBT *rbt = new RBT();
+  int result = rbt->comparator(comp1, comp2);
+  EXPECT_EQ(result, -1);
+}
+
+TEST(RBTComparator, Test3) {
+  pair<int, string> comp1(12210795, "Algorithm");
+  pair<int, string> comp2(12210795, "Misaso");
+
+  RBT *rbt = new RBT();
+  int result = rbt->comparator(comp1, comp2);
+  EXPECT_EQ(result, 1);
+}
+
+TEST(RBTComparator, Test4) {
+  pair<int, string> comp1(12210795, "Misaso");
+  pair<int, string> comp2(12210795, "Algorith");
+
+  RBT *rbt = new RBT();
+  int result = rbt->comparator(comp1, comp2);
+  EXPECT_EQ(result, -1);
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+/*
+[==========] Running 4 tests from 1 test suite.
+[----------] Global test environment set-up.
+[----------] 4 tests from RBTComparator
+[ RUN      ] RBTComparator.Test1
+[       OK ] RBTComparator.Test1 (0 ms)
+[ RUN      ] RBTComparator.Test2
+[       OK ] RBTComparator.Test2 (0 ms)
+[ RUN      ] RBTComparator.Test3
+[       OK ] RBTComparator.Test3 (0 ms)
+[ RUN      ] RBTComparator.Test4
+[       OK ] RBTComparator.Test4 (0 ms)
+[----------] 4 tests from RBTComparator (0 ms total)
+
+[----------] Global test environment tear-down
+[==========] 4 tests from 1 test suite ran. (0 ms total)
+[  PASSED  ] 4 tests.
+*/
